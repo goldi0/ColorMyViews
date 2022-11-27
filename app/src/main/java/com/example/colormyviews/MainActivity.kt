599fc589d94618a7fce1b4682ccd0f8cb5bf29b4
goldi meshram <gldmeshram@gmail.com>
2022-11-27 16:05:25 +0530

    finished project

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import
 
 
 class MainActivity : AppCompatActivity() {
